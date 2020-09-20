996fac65d76a8d71ddc5c6f3442675f79dd8140e
Rajkumar Soni <sonirajkumar031@gmail.com>
2020-09-20 16:03:04 +0530

    navigation to home page added in tabs

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -5,80 +5,80 @@ import { AlertController, LoadingController } from '@ionic/angular';
 import { AuthService, AuthResponseData } from '../../auth/auth.service';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss'],
+    selector: 'app-signup',
+    templateUrl: './signup.page.html',
+    styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
 
-  isLoading = false;
-  show = false;
-  isClicked: boolean = false;
-  @ViewChild('f') form: NgForm;
-  email: string;
-  password: string;
-  constructor(private authService: AuthService ,
-     private loadingController :LoadingController,
-     private alertDialog : AlertController,
-     private router : Router
-     ) {
+    isLoading = false;
+    show = false;
+    isClicked: boolean = false;
+    @ViewChild('f') form: NgForm;
+    email: string;
+    password: string;
+    constructor(private authService: AuthService,
+        private loadingController: LoadingController,
+        private alertDialog: AlertController,
+        private router: Router
+    ) {
 
-   }
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
 
-  onClicked() {
-    this.isClicked = true;
-    console.log(this.isClicked);
-    this.email = this.form.value['email'];
-    console.log(this.email);
-  }
+    onClicked() {
+        this.isClicked = true;
+        console.log(this.isClicked);
+        this.email = this.form.value['email'];
+        console.log(this.email);
+    }
 
-  navigateToHomePage(){
-      this.router.navigate(['/']);
-  }
+    navigateToHomePage() {
+        this.router.navigate(['/tabs/tab-nav']);
+    }
 
-  onSubmit(form: NgForm) {
-    this.isLoading = true;
-    this.password = this.form.value['password'];
+    onSubmit(form: NgForm) {
+        this.isLoading = true;
+        this.password = this.form.value['password'];
 
-    this.loadingController.create({ keyboardClose: true, message: 'Registering' })
-      .then(loadingEl => {
+        this.loadingController.create({ keyboardClose: true, message: 'Registering' })
+            .then(loadingEl => {
 
-        loadingEl.present();
-        
-        this.authService.signup(this.email, this.password).subscribe(resData => {
-          this.isLoading = false;
-          loadingEl.dismiss();
-          
-          this.navigateToHomePage();
-        
-        },
-        errorRes => {
-            this.isLoading = false;
-            loadingEl.dismiss();
-            const code =  errorRes.error.error.message;
-            let message = "Could not Sign you in ! Please try again!!";
-            
-            if(code === 'EMAIL_EXISTS'){
-              message = 'This email is already registered yet!';
-            }
-            this.showAlert(message);
-        });
-      });
-            }
+                loadingEl.present();
 
-  navigateBack() {
+                this.authService.signup(this.email, this.password).subscribe(resData => {
+                    this.isLoading = false;
+                    loadingEl.dismiss();
+
+                    this.navigateToHomePage();
+
+                },
+                    errorRes => {
+                        this.isLoading = false;
+                        loadingEl.dismiss();
+                        const code = errorRes.error.error.message;
+                        let message = "Could not Sign you in ! Please try again!!";
+
+                        if (code === 'EMAIL_EXISTS') {
+                            message = 'This email is already registered yet!';
+                        }
+                        this.showAlert(message);
+                    });
+            });
+    }
+
+    navigateBack() {
         this.isClicked = false;
-      }
-  showPassword() {
+    }
+    showPassword() {
         this.show = !this.show;
-      }
+    }
 
-      private showAlert(message:string){
-        this.alertDialog.create({ header: "Authentication Failed", message: message , buttons : ['Okay']}).then(alertEl => alertEl.present());
-  }
+    private showAlert(message: string) {
+        this.alertDialog.create({ header: "Authentication Failed", message: message, buttons: ['Okay'] }).then(alertEl => alertEl.present());
+    }
 
 }
