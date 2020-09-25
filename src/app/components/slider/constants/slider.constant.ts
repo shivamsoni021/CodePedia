925d42c137123a429c9822bb578a896cd9eb395f
Rajkumar Soni <sonirajkumar031@gmail.com>
2020-09-26 01:59:00 +0530

    feat: slider component added

---
+++ src/app/components/slider/constants/slider.constant.ts
@@ -0,0 +1,41 @@
+/** Type of slider:
+ * PreviewOnly - Consists of only image
+ * PreviewWithDescription - Consists of image, name, description and rating
+ */
+export enum SliderType {
+    PreviewOnly = 'preview',
+    PreviewWithDescription = 'preview-desc'
+}
+
+/** Describes the orientation of slider */
+export enum SliderOrientation {
+    Horizontal = 'horizontal',
+    Vertical = 'vertical'
+}
+
+/** Slider item interface */
+export interface SliderItem {
+    uniqueId: string;
+    type: 'tag' | 'course';
+    imageSrc?: string;
+    imageClass?: string;
+    name?: string;
+    nameClass?: string;
+    description?: string;
+    descriptionClass?: string;
+    rating?: number;
+    ratingClass?: string;
+    ratingIcon?: string;
+    ratingIconClass?: string;
+    inWatchList?: boolean;
+    isWatched?: boolean;
+    mediaUuid?: string;
+}
+
+/** Slider configuration interface for slider component */
+export interface SliderConfiguration {
+    type: SliderType;
+    orientation: SliderOrientation;
+    items: Array<SliderItem>;
+    sliderOptions?: any;
+}
