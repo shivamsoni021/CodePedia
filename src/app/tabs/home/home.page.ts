94712e3e99687afbec0d04f0d5b46fd9e269e882
Rajkumar Soni <sonirajkumar031@gmail.com>
2020-10-17 18:16:42 +0530

    fix: lint issues fixed

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -20,33 +20,34 @@ export class HomePage implements OnInit {
     userId: string;
     technologyTitle = 'Shivam';
     technologies: SliderConfiguration;
-    isStudying=false;
+    isStudying = false;
     enrolledCourses: SliderConfiguration;
     suggestedCourses: SliderConfiguration;
-    coursePreference:string = "";
+    coursePreference = '';
     constructor(
         private authService: AuthService,
         private homeFormatterService: HomeFormatterService,
         private profileService: ProfileService,
-        private homeService : HomeDatabase,
-        private router : Router,
-        private databaseService : DatabaseService
+        private homeService: HomeDatabase,
+        private router: Router,
+        private databaseService: DatabaseService
     ) { }
 
-    ngOnInit() {  
+    ngOnInit() {
         this.userId = this.authService.getUserId();
         this.loadEnrolledCourse();
         this.databaseService.getCompletedCourses(this.userId);
-        this.loadSuggestedCourse();        
+        this.loadSuggestedCourse();
     }
 
-    loadEnrolledCourse(){
-        let temData = [];
-        this.profileService.getEnrolledCourse(this.userId).subscribe((resData:any)=>{
-            for(const course in resData){
-                this.homeService.loadEnrolledCourse(resData[course].courseId).subscribe((data:any)=>{
+    loadEnrolledCourse() {
+        const temData = [];
+        this.profileService.getEnrolledCourse(this.userId).subscribe((resData: any) => {
+            // tslint:disable-next-line: forin
+            for (const course in resData) {
+                this.homeService.loadEnrolledCourse(resData[course].courseId).subscribe((data: any) => {
                     temData.push(data);
-                    if(temData.length) {
+                    if (temData.length) {
                         this.enrolledCourses = this.homeFormatterService.getFormattedTechnologyData(temData);
                     }
                 });
@@ -54,40 +55,41 @@ export class HomePage implements OnInit {
         });
     }
 
-    loadSuggestedCourse(){
-        let temData = [];
-        this.homeService.loadSuggestedCourse().subscribe((resData:any)=>{
-            for(const course in resData){
-                this.homeService.loadEnrolledCourse(resData[course].courseId).subscribe((data:any)=>{
+    loadSuggestedCourse() {
+        const temData = [];
+        this.homeService.loadSuggestedCourse().subscribe((resData: any) => {
+            // tslint:disable-next-line: forin
+            for (const course in resData) {
+                this.homeService.loadEnrolledCourse(resData[course].courseId).subscribe((data: any) => {
                     temData.push(data);
                     console.log(temData);
-                    if(temData.length){
+                    if (temData.length) {
                         this.suggestedCourses = this.homeFormatterService.getFormattedTechnologyData(temData);
                         console.log(this.suggestedCourses);
                     }
-               });
+                });
             }
         });
     }
-  
+
     navigateToCoursePage(courseDetails) {
-        let courseType = courseDetails.courseType;
+        const courseType = courseDetails.courseType;
         this.router.navigate(['tabs/courses/course-details'], {
             state: {
                 courseDetails,
                 courseType
             }
-        });    
+        });
     }
 
-    navigation(preference){
-        if(preference == "all-courses"){
-            this.coursePreference = "all-courses";
+    navigation(preference) {
+        if (preference === 'all-courses') {
+            this.coursePreference = 'all-courses';
         }
-        else{
-            this.coursePreference = "studying";
+        else {
+            this.coursePreference = 'studying';
         }
-        let coursePreference = preference;
+        const coursePreference = preference;
         this.router.navigate(['tabs/courses'], {
             state: {
                 coursePreference
